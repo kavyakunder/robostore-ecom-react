3e9f178031c22a68bd5b864583c201d5bf1391f8
kavya_kunder <kavyakunder7082@gmail.com>
2022-04-07 10:24:06 +0530

    refactor:add route to login pages

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,6 +4,8 @@ import HomePage from "../pages/Home/landingPage";
 import ProductsPage from "../pages/Products/ProductsPage";
 import WishlistPage from "../pages/Wishlist/Wishlist";
 import CartPage from "../pages/CartPage/CartPage";
+import Login from "../pages/Login/Login";
+import Logout from "../pages/Logout/Logout";
 export default function SiteRoutes() {
   return (
     <Routes>
@@ -12,6 +14,8 @@ export default function SiteRoutes() {
       <Route path="/wishlist" element={<WishlistPage />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="/mockman" element={<Mockman></Mockman>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/logout" element={<Logout />} />
     </Routes>
   );
 }
