91b3c8524ced23bd739d8d922c771eb8f2e296b0
kavya_kunder <kavyakunder7082@gmail.com>
2022-07-30 23:15:25 +0530

    feat-search

--- src/components/Product List/ProductList.jsx
+++ src/components/Product List/ProductList.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ProductCard from "../Single Product/ProductCard";
 import Filters from "../Filters/Filters";
+import { useState } from "react";
 import "./product-list.css";
 
 function ProductList({ products }) {
+  const [inputData, setInputData] = useState("");
+
   return (
     <>
       <div className="main-container">
@@ -11,13 +14,32 @@ function ProductList({ products }) {
         <div className="category-navbar">
           <div className="category-bar">
             Showing {products.length} out of 15 results
+            <div>
+              Search:{" "}
+              <input
+                className="padding-top"
+                type="text"
+                placeholder="For eg:breadboard"
+                onChange={(e) => {
+                  setInputData(e.target.value);
+                }}
+              />
+            </div>
           </div>
           <div className="container">
             {console.log("products is", products)}
             {products.length > 0 ? (
-              products.map((item) => {
-                return <ProductCard product={item} key={item._id} />;
-              })
+              products
+                .filter((item) => {
+                  if (inputData === "") {
+                    return item;
+                  } else if (item.name.toLowerCase().includes(inputData)) {
+                    return item;
+                  }
+                })
+                .map((item) => {
+                  return <ProductCard product={item} key={item._id} />;
+                })
             ) : (
               <h2>No Products found</h2>
             )}
